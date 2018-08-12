95b6ed955de8fe2766fbf1160fbacbe99dd1e1dd
oscarzhang795 <oscarzhang795@gmail.com>
2018-08-12 10:32:53 -0700

    Applied recycler view

--- app/src/main/java/com/example/oscar/sanguoshaenglish/Fragments/CharactersFragment.kt
+++ app/src/main/java/com/example/oscar/sanguoshaenglish/Fragments/CharactersFragment.kt
@@ -2,19 +2,31 @@ package com.example.oscar.sanguoshaenglish.Fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.oscar.sanguoshaenglish.R
+import kotlinx.android.synthetic.main.fragment_characters.*
 
 class CharactersFragment : Fragment() {
-    private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_characters, container, false)
+        viewManager = GridLayoutManager(this.context, 5)
+        viewAdapter = CharactersAdapter(TODO("Create dataset"))
+
+        rv_char_list.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
         return view
     }
+
 }
