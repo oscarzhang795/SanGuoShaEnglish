e1baa83828390813cb38025a820fca424ed8c516
oscarzhang795 <oscarzhang795@gmail.com>
2018-08-23 19:46:41 -0700

    Got recycler view working with images

--- app/src/main/java/com/example/oscar/sanguoshaenglish/Fragments/CharactersAdapter.kt
+++ app/src/main/java/com/example/oscar/sanguoshaenglish/Fragments/CharactersAdapter.kt
@@ -1,22 +1,23 @@
 package com.example.oscar.sanguoshaenglish.Fragments
 
+import android.graphics.Bitmap
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.example.oscar.sanguoshaenglish.R
 
-class CharactersAdapter(private val cards: ArrayList<ImageView>) : RecyclerView.Adapter<CharactersAdapter.ViewHolder>() {
+class CharactersAdapter(private val cards: ArrayList<Bitmap>) : RecyclerView.Adapter<CharactersAdapter.ViewHolder>() {
 
     class ViewHolder(val imageView: ImageView) : RecyclerView.ViewHolder(imageView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val imageView = LayoutInflater.from(parent.context).inflate(R.layout.imageview_card, parent, false) as ImageView
+        val imageView = LayoutInflater.from(parent.context).inflate(R.layout.imageview_cards, parent, false) as ImageView
         return ViewHolder(imageView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.imageView.setImageResource(cards.get(position).id)
+        holder.imageView.setImageBitmap(cards[position])
     }
 
     override fun getItemCount() = cards.size
--- app/src/main/java/com/example/oscar/sanguoshaenglish/Fragments/CharactersFragment.kt
+++ app/src/main/java/com/example/oscar/sanguoshaenglish/Fragments/CharactersFragment.kt
@@ -1,13 +1,15 @@
 package com.example.oscar.sanguoshaenglish.Fragments
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.oscar.sanguoshaenglish.R
 import kotlinx.android.synthetic.main.fragment_characters.*
 
@@ -15,18 +17,39 @@ class CharactersFragment : Fragment() {
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_characters, container, false)
+
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         viewManager = GridLayoutManager(this.context, 5)
-        viewAdapter = CharactersAdapter(TODO("Create dataset"))
+
+
+        val list = ArrayList<Bitmap>()
+        val test = BitmapFactory.decodeResource(resources, R.drawable.image001)
+        val test2 = ImageView(this.context)
+        test2.setImageBitmap(test)
+        for(i in 1..10) {
+            list.add(test)
+        }
+        viewAdapter = CharactersAdapter(list)
 
         rv_char_list.apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
         }
-
-        return view
     }
-
 }
