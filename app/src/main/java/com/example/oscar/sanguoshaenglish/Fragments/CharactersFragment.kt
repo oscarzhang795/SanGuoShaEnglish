16fb6c5a5d5d88a35db55c60467c01dfdf7d323d
oscarzhang795 <oscarzhang795@gmail.com>
2018-10-08 20:09:50 -0700

    Objectbox data works

--- app/src/main/java/com/example/oscar/sanguoshaenglish/Fragments/CharactersFragment.kt
+++ app/src/main/java/com/example/oscar/sanguoshaenglish/Fragments/CharactersFragment.kt
@@ -1,9 +1,7 @@
 package com.example.oscar.sanguoshaenglish.Fragments
 
 import android.os.Bundle
-import android.provider.Settings
 import android.support.v4.app.Fragment
-import android.support.v7.recyclerview.R.attr.layoutManager
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -13,15 +11,17 @@ import android.view.ViewGroup
 import com.beust.klaxon.Klaxon
 import com.example.oscar.sanguoshaenglish.Activities.MainMenuActivity
 import com.example.oscar.sanguoshaenglish.Adapters.CharactersAdapter
+import com.example.oscar.sanguoshaenglish.Entities.Ability
+import com.example.oscar.sanguoshaenglish.Entities.Character
 import com.example.oscar.sanguoshaenglish.Entities.CharacterData
+import com.example.oscar.sanguoshaenglish.Entities.CharacterData_
 import com.example.oscar.sanguoshaenglish.R
-import com.example.oscar.sanguoshaenglish.R.id.rv_char_list
 import com.example.oscar.sanguoshaenglish.SanGuoShaApplication
 import io.objectbox.Box
 import io.objectbox.kotlin.boxFor
+import kotlinx.android.synthetic.main.fragment_characters.*
 import kotlinx.coroutines.experimental.GlobalScope
 import kotlinx.coroutines.experimental.launch
-import kotlin.concurrent.thread
 
 
 class CharactersFragment : Fragment() {
@@ -29,7 +29,7 @@ class CharactersFragment : Fragment() {
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var characterBox: Box<CharacterData>
 
-    val shuCharacters = ArrayList<CharacterData>()
+    val shuCharacters = ArrayList<Character>()
     val wuCharacters = ArrayList<CharacterData>()
     val weiCharacters = ArrayList<CharacterData>()
     val kingdomelesssCharacters = ArrayList<CharacterData>()
@@ -38,9 +38,16 @@ class CharactersFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity as MainMenuActivity).supportActionBar?.title = "Characters"
-        ((activity as MainMenuActivity).application as SanGuoShaApplication).boxStore.boxFor<CharacterData>()
+        characterBox = ((activity as MainMenuActivity).application as SanGuoShaApplication).boxStore.boxFor<CharacterData>()
 
-        loadJson()
+//        loadJson()
+        val testChara = characterBox.query().equal(CharacterData_.alignment, "WU").build().find()
+//        testChara.get(0).abilities.get(0).description
+        Log.i("Ability name", testChara.get(0).abilities.get(0).description)
+//        testChara.forEach {
+//            shuCharacters.add(Character(it, resources.getIdentifier(it.image_name, "drawable", context!!.packageName)))
+//            Log.i("Image name", it.abilities[0].description)
+//        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -51,13 +58,13 @@ class CharactersFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        viewManager = GridLayoutManager(this.context, 3)
-//        viewAdapter = CharactersAdapter(allCharacters, this.context!!)
-//        rv_char_list.apply {
-//            setHasFixedSize(true)
-//            adapter = viewAdapter
-//            layoutManager = viewManager
-//        }
+        viewManager = GridLayoutManager(this.context, 3)
+        viewAdapter = CharactersAdapter(shuCharacters, this.context!!)
+        rv_char_list.apply {
+            setHasFixedSize(true)
+            adapter = viewAdapter
+            layoutManager = viewManager
+        }
     }
 
     private fun loadJson() {
@@ -67,12 +74,13 @@ class CharactersFragment : Fragment() {
         GlobalScope.launch {
             val parsed = klaxon.parseArray<CharacterData>(inputStream)
             parsed?.forEach { it: CharacterData ->
-                when(it.alignment){
-                    "Shu" -> shuCharacters.add(it)
-                    "Wei" -> weiCharacters.add(it)
-                    "Wu" -> wuCharacters.add(it)
-                    "Kingdomless" -> kingdomelesssCharacters.add(it)
-                }
+//                when(it.alignment){
+//                    "Shu" -> shuCharacters.add(it)
+//                    "Wei" -> weiCharacters.add(it)
+//                    "Wu" -> wuCharacters.add(it)
+//                    "Kingdomless" -> kingdomelesssCharacters.add(it)
+//                }
+                Log.i("Image name", it.abilities[0].description)
                 characterBox.put(it)
                 allCharacters.add(it)
             }
